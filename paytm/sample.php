2b671dfb6888ab3f5dc2f513648801116a25a597
4slots <77054903+4slots@users.noreply.github.com>
2021-01-10 09:02:58 -0500

    Update sample.php
    asdsad

--- paytm/sample.php
+++ paytm/sample.php
@@ -7,12 +7,12 @@ require_once("./PaytmChecksum.php");
 $paytmParams = array();
 
 /* add parameters in Array */
-$paytmParams["MID"] = "YOUR_MID_HERE";
+$paytmParams["MID"] = "ZaKhJF45180846090084";
 $paytmParams["ORDERID"] = "YOUR_ORDERID_HERE";
 
 /**
 * Generate checksum by parameters we have
 * Find your Merchant Key in your Paytm Dashboard at https://dashboard.paytm.com/next/apikeys 
 */
-$paytmChecksum = PaytmChecksum::generateSignature($paytmParams, 'YOUR_MERCHANT_KEY');
+$paytmChecksum = PaytmChecksum::generateSignature($paytmParams, 'lw1M@U3lmwkoQ7PU');
 echo sprintf("generateSignature Returns: %s\n", $paytmChecksum);
